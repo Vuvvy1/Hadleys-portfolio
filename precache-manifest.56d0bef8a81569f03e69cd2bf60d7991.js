6ce546a9d324f0885ee24654791573e6d65e8564
Hadley Sutherland <sutherlandhadley@gmail.com>
2023-03-24 17:29:29 -0600

    Updates

---
+++ precache-manifest.56d0bef8a81569f03e69cd2bf60d7991.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45b3a1d9e1692e7e1672dd651765a33",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "dc76008ccb1c00dabf2f",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "c812344c10d32c9effe5",
+    "url": "/react-frontend-dev-portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "dc76008ccb1c00dabf2f",
+    "url": "/react-frontend-dev-portfolio/static/js/2.020b9da4.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.020b9da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c812344c10d32c9effe5",
+    "url": "/react-frontend-dev-portfolio/static/js/main.2f45dce0.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  }
+]);
