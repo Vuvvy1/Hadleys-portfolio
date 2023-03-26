c80198bddaa5cf209571e71e9bc7c78f2539bdde
Hadley Sutherland <sutherlandhadley@gmail.com>
2023-03-26 15:10:13 -0600

    scss to fit on screen better

---
+++ build/precache-manifest.dfc0c3b1264925cd4e41490a3da5885c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19dd411eb9941fb1afdaf766cbe3c3a5",
+    "url": "/Hadleys-portfolio/index.html"
+  },
+  {
+    "revision": "dc76008ccb1c00dabf2f",
+    "url": "/Hadleys-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "ee44716a091ede0e8038",
+    "url": "/Hadleys-portfolio/static/css/main.fe19e681.chunk.css"
+  },
+  {
+    "revision": "dc76008ccb1c00dabf2f",
+    "url": "/Hadleys-portfolio/static/js/2.020b9da4.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Hadleys-portfolio/static/js/2.020b9da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee44716a091ede0e8038",
+    "url": "/Hadleys-portfolio/static/js/main.092200c1.chunk.js"
+  },
+  {
+    "revision": "207d1ca6d260d5a4c0b9",
+    "url": "/Hadleys-portfolio/static/js/runtime-main.cdc3e7d6.js"
+  }
+]);
